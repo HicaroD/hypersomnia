c7e8fc68c5ae37c758669d5f22a7b83f412825ed
HicaroD <hdanrlley1@gmail.com>
2024-06-04 12:50:50 -0300

    feat(colors): new color for background

--- colors.go
+++ colors.go
@@ -2,4 +2,5 @@ package main
 
 import "github.com/gdamore/tcell/v2"
 
-var BACKGROUND_COLOR tcell.Color = tcell.NewRGBColor(28, 28, 28)
+var WELCOME_DARK_BACKGROUND tcell.Color = tcell.NewRGBColor(28, 28, 28)
+var DARK_GREY tcell.Color = tcell.NewRGBColor(37, 34, 35)
