668ff3aa820ef506e104bb3c64424c35834c6ab4
HicaroD <hdanrlley1@gmail.com>
2024-07-03 09:24:05 -0300

    refactor: commenting code related to keyboard actions

--- navigator/navigator.go
+++ navigator/navigator.go
@@ -1,8 +1,6 @@
 package navigator
 
 import (
-	"fmt"
-
 	db "github.com/HicaroD/hypersomnia/database"
 	"github.com/HicaroD/hypersomnia/pages"
 
@@ -27,7 +25,7 @@ type Navigator struct {
 func New(p *tview.Pages, database *db.Database) *Navigator {
 	navigator := Navigator{
 		pages:        p,
-		pageManager:  pages.New(),
+		pageManager:  pages.New(database),
 		currentPage:  -1,
 		previousPage: -1,
 	}
@@ -35,7 +33,7 @@ func New(p *tview.Pages, database *db.Database) *Navigator {
 }
 
 func (navigator *Navigator) Navigate(index pages.Index) error {
-	name, page, err := navigator.getPage(index)
+	name, page, err := navigator.pageManager.GetPage(index)
 	if err != nil {
 		return err
 	}
@@ -53,23 +51,4 @@ func (navigator *Navigator) ShowPopup(popup *tview.Flex) {
 	navigator.pages = navigator.pages.AddPage("popup", popup, true, true)
 	navigator.previousPage = navigator.currentPage
 	navigator.currentPage = pages.POPUP
-}
-
-func (navigator *Navigator) getPage(index pages.Index) (string, tview.Primitive, error) {
-	var page tview.Primitive
-	switch index {
-	case pages.WELCOME:
-		page = navigator.pageManager.Welcome.Page()
-	case pages.HELP:
-		page = navigator.pageManager.Help.Page()
-	default:
-		return "", nil, fmt.Errorf("unimplemented page: %s", index)
-	}
-
-	name, ok := pages.NAMES[index]
-	if !ok {
-		return "", nil, fmt.Errorf("page '%s' name not found", index)
-	}
-
-	return name, page, nil
 }
--- pages/pages.go
+++ pages/pages.go
@@ -1,6 +1,11 @@
 package pages
 
-import "github.com/rivo/tview"
+import (
+	"fmt"
+
+	"github.com/rivo/tview"
+	db "github.com/HicaroD/hypersomnia/database"
+)
 
 type Page interface {
 	Setup()
@@ -39,24 +44,44 @@ var NAMES map[Index]string = map[Index]string{
 
 type PageManager struct {
 	Welcome *WelcomePage
-	// Endpoints *EndpointsPage
+	Endpoints *EndpointsPage
 	Help *HelpPage
 }
 
-func New() *PageManager {
+func New(database *db.Database) *PageManager {
 	// NOTE: should I initialize everything all at once?
 	welcome := &WelcomePage{}
 	welcome.Setup()
 
-	// endpoints := &EndpointsPage{}
-	// endpoints.Setup()
+	endpoints := &EndpointsPage{db: database}
+	endpoints.Setup()
 
 	help := &HelpPage{}
 	help.Setup()
 
 	return &PageManager{
 		Welcome: welcome,
-		// Endpoints: endpoints,
-		Help:      help,
+		Endpoints: endpoints,
+		Help: help,
 	}
+}
+
+func (pm *PageManager) GetPage(index Index) (string, tview.Primitive, error) {
+	var page Page
+	switch index {
+	case WELCOME:
+		page = pm.Welcome
+	case HELP:
+		page = pm.Help
+	case ENDPOINTS:
+		page = pm.Endpoints
+	default:
+		return "", nil, fmt.Errorf("unimplemented page: %s", index)
+	}
+
+	name, ok := NAMES[index]
+	if !ok {
+		return "", nil, fmt.Errorf("page '%s' name not found", index)
+	}
+	return name, page.Page(), nil
 }
