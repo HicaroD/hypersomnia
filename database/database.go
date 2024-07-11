60e5162b17c04845a0903d57b52d764e115c5b98
HicaroD <hdanrlley1@gmail.com>
2024-07-11 14:42:38 -0300

    chore: adding new comment for the future

--- database/database.go
+++ database/database.go
@@ -30,6 +30,7 @@ func New(dbPath string) (*Database, error) {
 
 func (db *Database) AddNewCollection(name string) error {
 	// TODO: add new collection to database
+	// See https://go.dev/doc/database/sql-injection for avoiding SQL injection
 	return nil
 }
 
